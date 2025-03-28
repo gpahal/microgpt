eff9795e0ae63e32b7d39e208308201d0e71ee98
Garvit Pahal <g10pahal@gmail.com>
2025-03-29 03:49:17 +0530

    Initialize repo

--- scripts/model/combine_model_stage2_runs.py
+++ scripts/model/combine_model_stage2_runs.py
@@ -1,14 +1,17 @@
+import asyncio
 import os
 import shutil
 
 import torch
 
+from microgpt.common.device import _get_device
 from microgpt.common.logger import _new_logger
+from microgpt.model.model import CustomTrainedModelConfig, Model
 
 logger = _new_logger(__name__)
 
 
-def main() -> None:
+async def main() -> None:
     dirname = os.path.dirname(os.path.abspath(__file__))
     trained_model_dir_path = os.path.join(dirname, "trained_model_stage2")
     run_dirs = os.listdir(trained_model_dir_path)
@@ -21,7 +24,8 @@ def main() -> None:
     output_dir_path = os.path.join(trained_model_dir_path, "output")
     os.makedirs(output_dir_path, exist_ok=True)
 
-    model_weights: dict[str, torch.Tensor] = {}
+    model: Model | None = None
+    device = _get_device()
     for run_idx, run in enumerate(run_ints):
         run_output_dir_path = os.path.join(trained_model_dir_path, f"run_{run}/output")
         assert os.path.exists(run_output_dir_path), f"Trained model directory not found: {run_output_dir_path}"
@@ -61,16 +65,31 @@ def main() -> None:
         for file_name, new_file_name in file_names_to_new_file_names_map.items():
             shutil.copy(os.path.join(run_output_dir_path, file_name), os.path.join(output_dir_path, new_file_name))
 
-        run_model_weights = torch.load(os.path.join(run_output_dir_path, "model.pt"), weights_only=True)
-        if run_idx == 0:
-            for key, value in run_model_weights.items():
-                model_weights[key] = value / len(run_ints)
+        run_model_weights: dict[str, torch.Tensor] = torch.load(
+            os.path.join(run_output_dir_path, "model.pt"), map_location=device, weights_only=True
+        )
+        if model is None:
+            model = await Model.load(config=CustomTrainedModelConfig(dir_path=run_output_dir_path), device=device)
         else:
+            model_weights = model.state_dict()
+            assert len(model_weights) == len(run_model_weights), (
+                f"Model weights must have the same length: {len(model_weights)} != {len(run_model_weights)}"
+            )
             for key, value in run_model_weights.items():
-                model_weights[key] += value / len(run_ints)
+                assert key in model_weights, f"Key {key} not found in model weights"
+                assert model_weights[key].shape == value.shape, (
+                    f"Shape mismatch for key {key}: {model_weights[key].shape} != {value.shape}"
+                )
+                model_weights[key] += value
+            model.load_state_dict(model_weights)
 
-    torch.save(model_weights, os.path.join(output_dir_path, "model.pt"))
+    model_weights = model.state_dict()
+    for key, value in model_weights.items():
+        model_weights[key] = value / len(run_ints)
+    model.load_state_dict(model_weights)
+
+    torch.save(model.state_dict(), os.path.join(output_dir_path, "model.pt"))
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
